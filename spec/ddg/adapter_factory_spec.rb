cf0c6691ad98712d32c1a2bc34036b419e557f6a
Brandon Powers <brandon-powers@users.noreply.github.com>
2018-07-17 01:41:15 -0400

    Add AdapterFactory testing for PostgreSQL/Redshift

--- spec/ddg/adapter_factory_spec.rb
+++ spec/ddg/adapter_factory_spec.rb
@@ -3,14 +3,36 @@
 require 'ddg/adapter_factory'
 
 RSpec.describe DDG::AdapterFactory do
+  before(:all) do
+    system('bundle exec rake db:setup:postgresql')
+  end
+
+  after(:all) do
+    system('bundle exec rake db:teardown:postgresql')
+  end
+
+  let(:config) do
+    {
+      database: ENV['TEST_DATABASE'],
+      user: ENV['TEST_USER'],
+      host: ENV['TEST_HOST'],
+      port: ENV['TEST_PORT'],
+      password: ENV['TEST_PASSWORD']
+    }
+  end
+
   describe '::adapter' do
     context 'when PostgreSQL is passed with valid configuration' do
       it 'returns an instance of DDG::Adapter::PostgreSQL' do
+        adapter = DDG::AdapterFactory.adapter(:postgresql, config)
+        expect(adapter).to be_a(DDG::Adapter::PostgreSQL)
       end
     end
 
     context 'when Redshift is passed with valid configuration' do
       it 'returns an instance of DDG::Adapter::PostgreSQL' do
+        adapter = DDG::AdapterFactory.adapter(:redshift, config)
+        expect(adapter).to be_a(DDG::Adapter::PostgreSQL)
       end
     end
 
